f041c5a19e9c353d20dbe1cac6da60e6a0710423
Pearl Latteier <pearlbea@gmail.com>
2024-07-19 14:24:06 -0500

    Start speaking section

---
+++ app/speaking/layout.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { clsx } from "clsx";
+import { roboto } from "../fonts";
+import NavMenu from "../nav";
+
+export default function ArticleLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main className={clsx(roboto.className, "p-8")}>
+      <NavMenu />
+      {children}
+    </main>
+  );
+}
--- app/speaking/page.tsx
+++ app/speaking/page.tsx
@@ -1,13 +1,115 @@
-'use client';
+import { clsx } from "clsx";
+import Talk from "./talk";
+import { roboto_slab } from "../fonts";
 
-import NavMenu from "../nav";
-
-export default function Speaking(){
-    return (
-        <>
-        <NavMenu />
-        <h1>Speaking</h1>
-        </>
-
-    )
+export default function Speaking() {
+  return (
+    <>
+      <h1 className={clsx(roboto_slab.className, "text-4xl mb-8")}>Speaking</h1>
+      <Talk
+        title="Leverage the Power of Native with Progressive Web Apps"
+        event="Girl Geek X, Virtual Girl Geek Dinner"
+        date="Oct 8, 2020"
+        link="https://girlgeek.io/events/codesee-girl-geek-event-networking/"
+      />
+      <Talk
+        title="Making Our Websites Load Faster"
+        event="Propeller Health Lunch and Learn, Madison, WI"
+        date="Sep 1, 2020"
+        link="https://docs.google.com/presentation/d/1XPnWUbtv53SWdAvGDGhej9pSC75jremWJpcS7vbnna0/edit?usp=sharing"
+      />
+      <Talk
+        title="PWAs with Angular"
+        event="MadJS Meetup, Madison, WI"
+        date="Jan 13, 2020"
+        link="https://docs.google.com/presentation/d/163gy77UhyL6yar7XSA1U1ynruKdUQuwwgiGIjdnvHcg/edit#slide=id.g6be481fde4_0_1082"
+      />
+      <Talk
+        title="Making Our Products Accessible"
+        event="Propeller Health Lunch and Learn, Madison, WI"
+        date="Oct 30, 2019"
+        link="https://docs.google.com/presentation/d/1Z4jL1oY-guo5ZSt2uar1E6Bdk2FJPcZjfM6vfJBNpRM/edit?usp=sharing"
+      />
+      <Talk
+        title="Web Components: Building Blocks of the Web"
+        event="Forward Technology Conference, Madison, WI"
+        date="Aug 26, 2019"
+        link="https://docs.google.com/presentation/d/1abeZip1A6uoK26E_tdo5jm1my6llRXB8vLt6PucyzWk/edit#slide=id.g502db9c2c7_0_0"
+      />
+      <Talk
+        title="Wicked Fast 🔥 Mobile Web"
+        event="THAT Conference, Wisconsin Dells, WI"
+        date="Aug 8, 2019"
+        link="https://docs.google.com/presentation/d/18ADMTCw3Z2XX4yrnzstuTt-9iDFf6j5DH-Xr1WZtEQY/edit?usp=sharing"
+      />
+      More info Wicked Fast 🔥 Mobile Web Codecinella, Madison, WI Aug 1, 2019
+      More info Conference Proposal Writing Workshop GDG Madison, Madison, WI
+      Jun 22, 2019 More info Get Flapping With Flutter REFACTR.TECH, Atlanta, GA
+      Jun 5, 2019 More info Flutter mobile app development study jam GDG
+      Madison, Madison, WI Jun 3, 2019 More info IWD Global Diversity CFP Day
+      Workshop GDG Madison & Madison WiT, Madison, WI Mar 2, 2019 More info It's
+      a Wonderful Web DevFestMN, Minneapolis, MN Feb 2, 2019 More info Get
+      Flapping With Flutter DevFestMN, Minneapolis, MN Feb 2, 2019 More info
+      It's a Wonderful Web! GDG Madison, Madison, WI Jan 15, 2019 More info How
+      Do Service Workers Even? Mobile Era, Oslo, Norway Nov 2, 2018 More info
+      Get Flapping with Flutter Mobile Era, Oslo, Norway Oct 31, 2018 More info
+      Get Flapping with Flutter GDG Madison, Madison, WI Oct 15, 2018 More info
+      Wicked Fast ⚡ Mobile Web Midwest JS, Minneapolis, MN Aug 8-10, 2018 More
+      info const name: Language = 'TypeScript'; Midwest JS, Minneapolis, MN Aug
+      8-10, 2018 More info Oh Node you didn’t! Go serverless with Cloud
+      Functions for Firebase GDG TC, Minneapolis, MN Aug 7, 2018 More info Web
+      Components: Lego bricks of the web We RISE Tech Conference, Atlanta, GA
+      Jun 21-22, 2018 More info Oh Node you didn’t! Go serverless with Cloud
+      Functions for Firebase MadJS, Madison, WI Jun 11, 2018 More info Google
+      I/O Extended Recap GDG Madison, Madison, WI Jun 15, 2018 More info PRPL on
+      Rails RailsConf, Pittsburgh, PA Apr 17-19, 2018 More info Wicked Fast ⚡
+      Mobile Web MilwaukeeJS, Milwaukee, WI Mar 27, 2018 More info Wicked Fast
+      ⚡ Mobile Web JazzCon.Tech, New Orleans, LA Mar 21-23, 2018 More info
+      Wicked Fast ⚡ Mobile Web DevFest KC, Kansas City, MO Feb 17, 2018 More
+      info Build, Test, and Deploy a Web Component with Nutmeg MadJS, Madison,
+      WI Feb 12, 2018 More info Wicked Fast ⚡ Mobile Web DevFest MN,
+      Minneapolis, MN Feb 10, 2018 More info Build, Test, and Deploy a Web
+      Component WindyCity DevFest, Chicago, IL Jan 12, 2018 More info Get
+      Talking with Actions on Google Ohio DevFest, Columbus, OH Nov 18, 2017
+      More info Energize Your Development Flow With CLIs DevOpsDays Madison,
+      Madison, WI Oct 31, 2017 More info Get Talking with Actions on Google
+      Mobile Era, Oslo, Norway Oct 5, 2017 More info Web Components: Lego bricks
+      of the web Connect.Tech, Atlanta, GA Sep 21, 2017 More info Energize your
+      development flow with CLIs Connect.Tech, Atlanta, GA Sep 21, 2017 More
+      info Material Design that Delights Connect.Tech, Atlanta, GA Sep 21, 2017
+      More info Material Design that Delights GDG Madison, Madison, WI Sep 16,
+      2017 More info Leverage the power of native with Progressive Web Apps
+      Midwest JS, Minneapolis, MN Aug 18, 2017 More info Web Components: Lego
+      Bricks of the Web Midwest JS, Minneapolis, MN Aug 17, 2017 More info Web
+      Components: Lego Bricks of the Web Rockford Web Devs, Rockford, IL Aug 8,
+      2017 More info Web Components: Lego Bricks of the Web KCDC, Kansas City,
+      MO Aug 3, 2017 More info Get the Power of Native with Progressive Web Apps
+      CoderCruise, Cozumel, Mexico Jul 22, 2017 More info Getting Started with
+      the Twitter API CoderCruise, Grand Cayman, Cayman Islands Jul 18, 2017
+      More info Web Components: Lego Bricks of the Web CoderCruise, Montego Bay,
+      Jamaica Jul 17, 2017 More info Leverage the power of native with
+      Progressive Web Apps MadJS, Madison, WI Jul 10, 2017 More info Google I/O
+      Recap GDG Madison, Madison, WI Jun 15, 2017 More info How to be a
+      professional developer (or just act like one) UW Madison CS Lecture,
+      Madison, WI Apr 27, 2017 More info Get talking with Actions on Google GDG
+      Madison, Madison, WI Apr 15, 2017 More info Web Components: Lego bricks of
+      the web JazzCon.Tech, New Orleans, LA Mar 22, 2017 More info Web
+      Components: Lego bricks of the web DevFest KC, Kansas City, MO Feb 18,
+      2017 More info Leverage the power of native with Progressive Web Apps
+      DevFest KC, Kansas City, MO Feb 18, 2017 More info Get talking with
+      Actions on Google DevFest KC, Kansas City, MO Feb 18, 2017 More info Get
+      talking with Actions on Google GDG Chicago, Chicago, IL Feb 8th, 2017 More
+      info Burn down your competitors with Firebase ConFoo Vancouver, Vancouver,
+      BC, CA Dec 5, 2016 More info Energize your development flow with CLIs
+      ConFoo Vancouver, Vancouver, BC, CA Dec 7, 2016 More info Web Components:
+      Lego bricks of the web DevFest West, Mountain View, CA Nov 3, 2016 More
+      info Leverage the power of native with Progressive Web Apps Mobile Era,
+      Oslo, Norway Nov 3, 2016 More info Web Components: Lego bricks of the web
+      Connect.Tech, Atlanta, GA Oct 20, 2016 More info Web Components: Lego
+      bricks of the web Madison PHP Conference, Madison, WI Oct 1, 2016 More
+      info Leverage the power of native with Progressive Web Apps Windy City
+      DevCon, Chicago, IL Sep 16, 2016 More info The Bendy Game of Life³
+      Madhackers, Madison, WI Jun 15, 2016 More info
+    </>
+  );
 }
