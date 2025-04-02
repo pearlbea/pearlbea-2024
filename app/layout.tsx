9b43a69162c69baf4b2e95a2718622ac6ca07172
Pearl Latteier <pearlbea@gmail.com>
2025-04-02 13:34:19 -0500

    Add Posthog

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { roboto, roboto_slab } from "./fonts";
 import "./globals.css";
 import Link from "next/link";
 import Image from "next/image";
+import { PostHogProvider } from "../components/PostHogProvider";
 
 export const metadata: Metadata = {
   title: "Pearl Latteier",
@@ -12,64 +13,64 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body className={clsx(roboto.className, "bg-slate-50")}>
-        <main className="container mx-auto max-w-5xl">
-          <header className="m-8">
-            <Image
-              src="/icon.png"
-              alt="PB icon"
-              width={60}
-              height={60}
-              priority
-            />
-            <h1 className={clsx(roboto_slab.className, "text-4xl mt-8")}>
-              Pearl Latteier
-            </h1>
-            <ul className="flex align-baseline">
-              <li className="m-1">
-                <Link
-                  aria-labelledby="gitHub"
-                  href="https://github.com/pearlbea"
-                  rel="noopener nofollow"
-                  target="_blank"
-                  className="text-pink-900 hover:text-pink-600 focus:text-pink-600 active:text-pink-600"
-                >
-                  github
-                </Link>
-              </li>
-              <li className="m-1">
-                <span className="mr-2">·</span>
-                <Link
-                  aria-labelledby="linkedIn"
-                  href="https://www.linkedin.com/in/pearllatteier/"
-                  rel="noopener nofollow"
-                  target="_blank"
-                  className="text-pink-900 hover:text-pink-600 focus:text-pink-600 active:text-pink-600"
-                >
-                  linikedIn
-                </Link>
-              </li>
-              <li className="m-1">
-                <span className="mr-2">·</span>
-                <Link
-                  aria-labelledby="twitter"
-                  href="https://twitter.com/pblatteier"
-                  rel="noopener nofollow"
-                  target="_blank"
-                  className="text-pink-900 hover:text-pink-600 focus:text-pink-600 active:text-pink-600"
-                >
-                  twitter
-                </Link>
-              </li>
-            </ul>
-          </header>
-          <div className="mx-8">{children}</div>
-        </main>
+      <body className={clsx(roboto.className, "bg-slate-50")}> 
+        <PostHogProvider>
+          <main className="container mx-auto max-w-5xl">
+            <header className="m-8">
+              <Image
+                src="/icon.png"
+                alt="PB icon"
+                width={60}
+                height={60}
+                priority
+              />
+              <h1 className={clsx(roboto_slab.className, "text-4xl mt-8")}>
+                Pearl Latteier
+              </h1>
+              <ul className="flex align-baseline">
+                <li className="m-1">
+                  <Link
+                    aria-labelledby="gitHub"
+                    href="https://github.com/pearlbea"
+                    rel="noopener nofollow"
+                    target="_blank"
+                    className="text-pink-900 hover:text-pink-600 focus:text-pink-600 active:text-pink-600"
+                  >
+                    github
+                  </Link>
+                </li>
+                <li className="m-1">
+                  <span className="mr-2">·</span>
+                  <Link
+                    aria-labelledby="linkedIn"
+                    href="https://www.linkedin.com/in/pearllatteier/"
+                    rel="noopener nofollow"
+                    target="_blank"
+                    className="text-pink-900 hover:text-pink-600 focus:text-pink-600 active:text-pink-600"
+                  >
+                    linikedIn
+                  </Link>
+                </li>
+                <li className="m-1">
+                  <span className="mr-2">·</span>
+                  <Link
+                    aria-labelledby="twitter"
+                    href="https://twitter.com/pblatteier"
+                    rel="noopener nofollow"
+                    target="_blank"
+                    className="text-pink-900 hover:text-pink-600 focus:text-pink-600 active:text-pink-600"
+                  >
+                    twitter
+                  </Link>
+                </li>
+              </ul>
+            </header>
+            <div className="mx-8">{children}</div>
+          </main>
+        </PostHogProvider>
       </body>
     </html>
   );
--- next.config.js
+++ next.config.js
@@ -4,4 +4,22 @@ const withMDX = require("@next/mdx")();
 
 module.exports = withMDX({
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+      {
+        source: "/ingest/decide",
+        destination: "https://us.i.posthog.com/decide",
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 });
